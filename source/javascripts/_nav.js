0b7bff2f1acadf8a94ba91e13d49909308942bac
Enrique <savir81@hotmail.com>
2017-12-14 10:19:49 -0800

    add new code

---
+++ source/javascripts/_nav.js
@@ -0,0 +1,20 @@
+// https://codepen.io/SmaragdaTh/pen/LERvNq
+$(document).ready(function() {
+  var n = '#nav', no = 'nav-open';
+  $('.nav-menu').click(function() {
+    if ($(n).hasClass(no)) {
+      //$(n).removeClass(no);
+      $(n).animate({height:0},300);
+      setTimeout(function(){
+        $(n).removeClass(no).removeAttr('style');
+      },320);
+    }
+    else {
+      var newH = $(n).css('height','auto').height();
+      $(n).height(0).animate({height:newH},300);
+      setTimeout(function(){
+        $(n).addClass(no).removeAttr('style');
+      },320); 
+    }
+  });
+});
