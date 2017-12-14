f9dd40542d4c85fc4768f001c39f18ba3fa76781
Enrique <savir81@hotmail.com>
2017-12-14 14:51:11 -0800

    add new code

--- config.rb
+++ config.rb
@@ -32,5 +32,28 @@ configure :build do
 	set :site_url, "/shop_store"
 end
 
+# environment variable with Snipcart API Key
+ENV["snipcart-api-key"] = "NWIxYmI4Y2QtMGRhOC00ZTdjLWI1YTctZmQ0NTI0MzJkM2NmNjM2Mjc0NTUxNDQwNTExMTM0"
+
+# environment variable to keep the base site URL
+ENV["base-url"] = "http://kikluz.com/shop_store/"
+ # creating a template helper to generate a Snipcart button.
+helpers do
+  def snipcart_button (p, text)
+    args = {
+      :"class" => "snipcart-add-item button is-primary is-medium",
+      :"data-item-id" => p.id,
+      :"data-item-price" => p.price,
+      :"data-item-name" => p.name,
+      :"data-item-max-quantity" => p.max_quantity,
+      :"data-item-url" => ENV["base-url"] + p.path,
+      :"data-item-image" => p.image
+    }
+
+    content_tag :button, args do
+      text
+    end
+  end
+end
 
 
